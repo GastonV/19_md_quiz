e61cb611fd5998dcb5840cae0076d972f4477ae0
GastonV <atis.sirmais@gmail.com>
2021-11-02 10:36:42 +0200

    problem with imports

---
+++ src/data/data.ts
@@ -0,0 +1,137 @@
+const questionsCSS = [
+  {
+    question: 'If we want define style for an unique element, then which css selector will we use ?',
+    options: {
+      A: 'Id',
+      B: 'text',
+      C: 'class',
+      D: 'name',
+    },
+    answer: 'A',
+  },
+  {
+    question: 'How will you make all paragraph elements \'RED\' in color ?',
+    options: {
+      A: 'p.all {color: red;}',
+      B: 'p.all {color: #990000;}',
+      C: 'all.p {color: #998877;}',
+      D: 'p {color: red;}',
+    },
+    answer: 'D',
+  },
+  {
+    question: 'If we want to show an Arrow as cursor, then which value we will use ?',
+    options: {
+      A: 'pointer',
+      B: 'default',
+      C: 'arrow',
+      D: 'arr',
+    },
+    answer: 'C',
+  },
+
+];
+
+// CSS**********************
+// If we want define style for an unique element, then which css selector will we use ?
+// A.Id
+// B.text
+// C.class
+// D.name
+
+// How will you make all paragraph elements 'RED' in color ?
+// A.p.all {color: red;}
+// B.p.all {color: #990000;}
+// C.all.p {color: #998877;}
+// D.p {color: red;}
+
+// If we want to show an Arrow as cursor, then which value we will use ?
+// A.pointer
+// B.default
+// C.arrow
+// D.arr
+
+const questionsHTML = [
+  {
+    question: 'When should you use path along with file name of picture in IMG tag?',
+    options: {
+      A: 'path is optional and not necessary',
+      B: 'when the location of image file andhtml file are different',
+      C: 'when image file and html file both are on same location',
+      D: 'path is always necessary when inserting image',
+    },
+    answer: 'D',
+  },
+  {
+    question: 'Which tag inserts a line horizontally on your web page?',
+    options: {
+      A: '<hr>',
+      B: '<line>',
+      C: '<line direction="horizontal">',
+      D: '<tr>',
+    },
+    answer: 'A',
+  },
+  {
+    question: '<SCRIPT> … </SCRIPT> tag can be placed within ________',
+    options: {
+      A: 'Header',
+      B: 'Body',
+      C: 'both A and B',
+      D: 'none of the above',
+    },
+    answer: 'A',
+  },
+
+];
+// When should you use path along with file name of picture in IMG tag?
+// a. path is optional and not necessary
+// b. when the location of image file andhtml file are different
+// c. when image file and html file both are on same location
+// d. path is always necessary when inserting image
+
+// Which tag inserts a line horizontally on your web page?
+// a. <hr>
+// b. <line>
+// c. <line direction="horizontal">
+// d. <tr>
+
+// <SCRIPT> … </SCRIPT> tag can be placed within ________
+// a. Header
+// b. Body
+// c. both A and B
+// d. none of the above
+const questionsTypeScript = [
+  {
+    question: 'If we want define style for an unique element, then which css selector will we use ?',
+    options: {
+      A: 'Id',
+      B: 'text',
+      C: 'class',
+      D: 'name',
+    },
+    answer: 'A',
+  },
+  {
+    question: 'How will you make all paragraph elements \'RED\' in color ?',
+    options: {
+      A: 'p.all {color: red;}',
+      B: 'p.all {color: #990000;}',
+      C: 'all.p {color: #998877;}',
+      D: 'p {color: red;}',
+    },
+    answer: 'D',
+  },
+  {
+    question: 'The advantages of Typescript are...',
+    options: {
+      A: 'It assists in code structuring.',
+      B: 'It applies class -based object oriented programming.',
+      C: 'It introduces coding guidelines.',
+      D: 'All of the above.',
+    },
+    answer: 'C',
+  }
+];
+
+export  {questionsCSS, questionsTypeScript, questionsHTML as default} ;
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,5 +1,127 @@
-const section = document.querySelector('.question');
+import questionsCSS from './data/data';
+import questionsHTML from './data/data';
+import bbb from './data/data';
 
-const questionAnswered = [];
 
-console.log('Ready for coding');
+
+const quizSection = document.querySelector('.quiz');
+
+const createAnswerElement = (answer:string) => {
+  const button = document.createElement('button');
+  button.classList.add('quiz__answer');
+  button.textContent = answer;
+  return button;
+};
+const createQuestionElement = (question:string) => {
+  const headingElement = document.createElement('h2');
+  const divelEment = document.createElement('div');
+  divelEment.appendChild(headingElement);
+  divelEment.classList.add('quiz__question');
+  headingElement.classList.add('quiz__heading2');
+  headingElement.textContent = question;
+  return divelEment;
+};
+
+const questionAnswered = {
+  question: 'If we want define style for an unique element, then which css selector will we use ?',
+  A: 'Id',
+  B: 'text',
+  C: 'class',
+  D: 'name',
+  correctOne: 1,
+};
+
+quizSection.appendChild(createQuestionElement(questionAnswered.question));
+quizSection.appendChild(createAnswerElement(questionAnswered.A));
+quizSection.appendChild(createAnswerElement(questionAnswered.B));
+quizSection.appendChild(createAnswerElement(questionAnswered.B));
+quizSection.appendChild(createAnswerElement(questionAnswered.C));
+
+document.getElementById('quiz').animate([
+  // keyframes
+  { transform: 'translateY(20px)' },
+  { transform: 'translateY(100px)' },
+  // { transform: 'rotate(15deg)' },
+], {
+  // timing options
+  duration: 1000,
+});
+
+const questionsHTML = [
+  {
+    question: 'When should you use path along with file name of picture in IMG tag?',
+    options: {
+      A: 'path is optional and not necessary',
+      B: 'when the location of image file andhtml file are different',
+      C: 'when image file and html file both are on same location',
+      D: 'path is always necessary when inserting image',
+    },
+    answer: 'D',
+  },
+  {
+    question: 'Which tag inserts a line horizontally on your web page?',
+    options: {
+      A: '<hr>',
+      B: '<line>',
+      C: '<line direction="horizontal">',
+      D: '<tr>',
+    },
+    answer: 'A',
+  },
+  {
+    question: '<SCRIPT> … </SCRIPT> tag can be placed within ________',
+    options: {
+      A: 'Header',
+      B: 'Body',
+      C: 'both A and B',
+      D: 'none of the above',
+    },
+    answer: 'A',
+  },
+
+];
+
+// Which of the following properties will we use to display border around a cell without any content ?
+// A.empty-cell
+// B.blank-cell
+// C.noncontent-cell
+// D.void-cell
+
+// HTML*****************
+// The way the browser displays the object can be modified by _____
+// a. attributes
+// b. parameters
+// c. modifiers
+// d. None of above
+
+// When should you use path along with file name of picture in IMG tag?
+// a. path is optional and not necessary
+// b. when the location of image file andhtml file are different
+// c. when image file and html file both are on same location
+// d. path is always necessary when inserting image
+
+// Which tag inserts a line horizontally on your web page?
+// a. <hr>
+// b. <line>
+// c. <line direction="horizontal">
+// d. <tr>
+
+// <SCRIPT> … </SCRIPT> tag can be placed within ________
+// a. Header
+// b. Body
+// c. both A and B
+// d. none of the above
+
+// Typescript *****************************************
+
+// The advantages of Typescript are...
+
+//  It assists in code structuring.
+//  It applies class -based object oriented programming.
+//  It introduces coding guidelines.
+//  All of the above.
+
+// Which one is incorrect method to declare
+// a variableA.let x=999;
+// B.var x:string =”string”;
+// C.let anyvar:any=[1,true,”name”]
